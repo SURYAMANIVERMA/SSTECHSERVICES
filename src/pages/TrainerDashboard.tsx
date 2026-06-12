0a02bcb6e88cfd3b3f780bf42ab71ec73a1a8398
SURYAMANIVERMA <57282468+SURYAMANIVERMA@users.noreply.github.com>
2026-06-12 22:38:24 +0530

    Create TrainerDashboard component for trainers

---
+++ src/pages/TrainerDashboard.tsx
@@ -0,0 +1,22 @@
+export default function TrainerDashboard() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Trainer Dashboard</h1>
+
+      <h2>Welcome Trainer ✅</h2>
+
+      <div>
+        <h3>Trainer Options</h3>
+
+        <ul>
+          <li>Upload Course Videos</li>
+          <li>Create Assignments</li>
+          <li>Create Quiz</li>
+          <li>Check Student Submissions</li>
+          <li>Mark Attendance</li>
+          <li>Manage Batch</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
